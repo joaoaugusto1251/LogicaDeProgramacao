59804765ad68472ee298dd603faa025f8b814ca5
joaoaugusto1251 <42012674+joaoaugusto1251@users.noreply.github.com>
2018-08-07 18:27:13 -0300

    Add files via upload

---
+++ PrimeiraQuestao.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PrimeiraQuestao {
+    public static void main(String[] args) {
+        Scanner digite = new Scanner (System.in);
+        int a = digite.nextInt();
+        int b = digite.nextInt();
+        
+        System.out.println(a + b);
+    }
+}
